f58b5219fd4031cd12f56ffff0b06395bbcb7bcc
Brasero <brandonricci4@gmail.com>
2025-03-21 11:58:02 +0100

    proposition de correction

--- app/src/features/api/apiSlice.js
+++ app/src/features/api/apiSlice.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api',
+    tagTypes: ["Pastries", "Auth"],
     baseQuery: fetchBaseQuery({ baseUrl: `${ import.meta.env.VITE_API_BASE }` }),
     endpoints: (builder) => ({
         /* Pastries */
